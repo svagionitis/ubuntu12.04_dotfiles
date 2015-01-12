30b7d8ea641024c019c7b61945a005899e04e9cf
Stavros Vagionitis <stavrosv@digisoft.tv>
2015-01-12 15:46:01 +0000

    Don't use the /home/stavros parameter, because when run not from command prompt produce an error.

--- .installed_packages/installed_packages_script.sh
+++ .installed_packages/installed_packages_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dpkg --get-selections > $HOME/.installed_packages/installed_packages.`date -u +%Y%m%d-%H%M%S`.pkg_list
+dpkg --get-selections > /home/stavros/.installed_packages/installed_packages.`date -u +%Y%m%d-%H%M%S`.pkg_list
 
