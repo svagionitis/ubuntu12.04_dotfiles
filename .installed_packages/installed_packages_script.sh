64cb7b1660fd9c2e27e02d62abc5b91bcc771661
Stavros Vagionitis <stavrosv@digisoft.tv>
2015-01-20 14:30:54 +0000

    Update the script to create a pip package list. Add the pip package list and the updated ubuntu package lists.

--- .installed_packages/installed_packages_script.sh
+++ .installed_packages/installed_packages_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 dpkg --get-selections > /home/stavros/.installed_packages/installed_packages.`date -u +%Y%m%d-%H%M%S`.pkg_list
+pip list > /home/stavros/.installed_packages/pip_installed_packages.`date -u +%Y%m%d-%H%M%S`.pip_pkg_list
 
