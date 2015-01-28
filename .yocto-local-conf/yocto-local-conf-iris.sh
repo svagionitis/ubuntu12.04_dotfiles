08354941ad4516f6d6cd5df4542468046de0387b
Stavros Vagionitis <stavrosv@digisoft.tv>
2015-01-28 17:55:00 +0000

    Leave only the directories

--- .yocto-local-conf/yocto-local-conf-iris.sh
+++ .yocto-local-conf/yocto-local-conf-iris.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 export BB_NUMBER_THREADS="16"
-export PARALLEL_MAKE="-j 8"
+export PARALLEL_MAKE="-j 16"
 
-export EXTRA_IMAGE_FEATURES="dbg-pkgs dev-pkgs tools-debug tools-profile tools-testapps debug-tweaks"
+#export EXTRA_IMAGE_FEATURES="dbg-pkgs dev-pkgs tools-debug tools-profile tools-testapps debug-tweaks"
 
 export DL_DIR="/media/part1/YoctoDownloadCommonArea/iris/downloads"
 export SSTATE_DIR="/media/part1/YoctoDownloadCommonArea/iris/sstate-cache"
 export IRIS_VERSION="0.0.0"
 export DEPLOY_DIR_IMAGE="/media/part1/YoctoDownloadCommonArea/iris/deploy"
 
-export GIT_USERNAME="stavros"
+#export GIT_USERNAME="stavros"
