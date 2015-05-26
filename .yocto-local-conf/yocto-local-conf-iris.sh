12b32e6482c9a1aec9d7c123ce523d1ad7a56a58
Stavros Vagionitis <stavrosv@digisoft.tv>
2015-05-26 17:22:24 +0100

    General changes

--- .yocto-local-conf/yocto-local-conf-iris.sh
+++ .yocto-local-conf/yocto-local-conf-iris.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export BB_NUMBER_THREADS="16"
-export PARALLEL_MAKE="-j 16"
+
+export BB_NUMBER_THREADS="32"
+export PARALLEL_MAKE="-j 32"
 
 #export EXTRA_IMAGE_FEATURES="dbg-pkgs dev-pkgs tools-debug tools-profile tools-testapps debug-tweaks"
 
@@ -9,5 +10,7 @@ export DL_DIR="/media/part1/YoctoDownloadCommonArea/iris/downloads"
 export SSTATE_DIR="/media/part1/YoctoDownloadCommonArea/iris/sstate-cache"
 export IRIS_VERSION="0.0.0"
 export DEPLOY_DIR_IMAGE="/media/part1/YoctoDownloadCommonArea/iris/deploy"
+
+export MACHINE="ddigital-SIP9-HD-STICK-V4"
 
 #export GIT_USERNAME="stavros"
